40351d13c4118f7c141bacf9cefdfd2bd819aa40
Rogério Vilela <rogerio@raccoon.ag>
2021-01-27 16:39:50 -0300

    add new visual component loading

--- src/styles/common.js
+++ src/styles/common.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
     margin-left: ${(props) => props.theme.container.mobile};
@@ -45,5 +45,40 @@ export const SectionSubtitle = styled.p`
         line-height: 35px;
         margin-bottom: 59px;
         text-align: left;
+    }
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 99;
+    background-color: rgba(240, 240, 240, 0.8);
+
+    &:after {
+        content: "";
+        border: 4px solid ${(props) => props.theme.colors.darkGray};
+        border-top: 4px solid ${(props) => props.theme.colors.orange};
+        border-radius: 50%;
+        width: 45px;
+        height: 45px;
+        animation: ${spin} 2s linear infinite;
     }
 `;
