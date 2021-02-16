c331787ea80847cfcbe3b03c03e02414f273a963
Victor <vhymathias@gmail.com>
2021-02-15 21:59:51 -0300

    file cleanup

--- src/components/CookieBar/styles.js
+++ src/components/CookieBar/styles.js
@@ -108,28 +108,3 @@ export const PolicyLink = styled.a`
         font-weight: bold;
     }
 `;
-
-export const UlComponent = styled.ul`
-    font-size: ${(props) => props.fontSize};
-    padding-inline-start: 0;
-    display: flex;
-    overflow: auto;
-    flex-direction: ${(props) => props.flexDirection};
-    justify-content: ${(props) => (props.justifyContent ? props.justifyContent : "space-around")};
-    align-items: flex-start;
-    li {
-        margin: ${(props) => (props.liMargin ? props.liMargin : "0")};
-        white-space: nowrap;
-        list-style-type: none;
-        color: #0b2747;
-        border-bottom: 3px solid transparent;
-        cursor: pointer;
-        transition: border-bottom 400ms linear;
-        transition: font-weight 300ms linear;
-    }
-    li.chosen {
-        border-bottom: 3px solid #37d6b5;
-        font-weight: bold;
-        cursor: default;
-    }
-`;
