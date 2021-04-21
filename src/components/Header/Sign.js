1321e76957fa55960a2235553c50bd2d7e46c91f
Victor Mathias <vhymathias@gmail.com>
2021-04-20 23:33:59 -0300

    Header: remoção do link de login para o MVP

--- src/components/Header/Sign.js
+++ src/components/Header/Sign.js
@@ -12,17 +12,12 @@ function Sign({ signInLink, signUpLink, className }) {
                     <UserIcon />
                 </AccountCircleButton>
                 <SignInButton href={signInLink}>Cadastrar</SignInButton>
-                <SignUpButton href={signUpLink}>
-                    <UserIcon />
-                    Login
-                </SignUpButton>
             </SignContainer>
             <SignMenu open={open}>
                 <AccountCircleButton onClick={() => setOpen(false)}>
                     <UserIcon />
                 </AccountCircleButton>
                 <SignMenuItem href={signInLink}>Cadastrar</SignMenuItem>
-                <SignMenuItem href={signUpLink}>Login</SignMenuItem>
             </SignMenu>
         </>
     );
