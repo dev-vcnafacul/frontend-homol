00275e40acc479407ae3715117ced6c726a001a9
Victor Mathias <vhymathias@gmail.com>
2021-04-23 21:55:28 -0300

    Apoiadores: fix da setinha do carrossel estar muito perto do triangulo de baixo

--- src/components/Supporters/styles.js
+++ src/components/Supporters/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Container, SectionTitle, SectionSubtitle } from "../../styles/common";
 
 export const Section = styled.div`
-    padding: 30px 0;
+    padding: 30px 0 50px 0;
     position: relative;
     display: block;
     overflow-x: hidden;
