c4a385fda1572443082588a41bc00b36fe54d9a1
Rogério Vilela <rogerio.junior@cashme.com.br>
2021-05-19 19:51:33 -0300

    add login url in header

--- src/components/Header/Sign.js
+++ src/components/Header/Sign.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ReactComponent as UserIcon } from "../../assets/icons/user.svg";
-import { SignContainer, SignInButton, AccountCircleButton, SignMenu, SignMenuItem } from "./styles";
+import { SignContainer, SignInButton, SignUpButton, AccountCircleButton, SignMenu, SignMenuItem } from "./styles";
 
 function Sign({ signInLink, solid, signUpLink, className }) {
     const [open, setOpen] = useState(false);
@@ -11,15 +11,20 @@ function Sign({ signInLink, solid, signUpLink, className }) {
                 <AccountCircleButton onClick={() => setOpen(true)}>
                     <UserIcon />
                 </AccountCircleButton>
-                <SignInButton solid={solid} href={signInLink}>
+                <SignInButton solid={solid} to={signInLink}>
                     Cadastrar
                 </SignInButton>
+                <SignUpButton to={signUpLink}>
+                    <UserIcon />
+                    Login
+                </SignUpButton>
             </SignContainer>
             <SignMenu open={open}>
                 <AccountCircleButton onClick={() => setOpen(false)}>
                     <UserIcon />
                 </AccountCircleButton>
-                <SignMenuItem href={signInLink}>Cadastrar</SignMenuItem>
+                <SignMenuItem to={signInLink}>Cadastrar</SignMenuItem>
+                <SignMenuItem to={signUpLink}>Login</SignMenuItem>
             </SignMenu>
         </>
     );
