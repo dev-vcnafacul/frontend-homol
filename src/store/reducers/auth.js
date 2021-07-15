33de2e804870ae68bb6cedbfcfc3dfd9e11a6e8b
Victor Hugo Y. Mathias <vhymathias@gmail.com>
2021-07-15 13:28:27 -0300

    finalized session persistance feature

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,9 +1,11 @@
-import { AUTH_SUCCESS } from "store/actions/auth.types";
+import { AUTH_SUCCESS, AUTH_CHECK_FROM_LS } from "store/actions/auth.types";
 import { UPDATE_ACCOUNT_SUCCESS } from "store/actions/account.types";
 
 export function authReducer(state = "", action) {
     switch (action.type) {
         case AUTH_SUCCESS:
+            return action.payload;
+        case AUTH_CHECK_FROM_LS:
             return action.payload;
         case UPDATE_ACCOUNT_SUCCESS:
             return { ...state, user: action.payload };
