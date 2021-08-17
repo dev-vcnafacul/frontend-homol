f07af2370aec5b843e25d3146f78852ed8ab7944
Victor Hugo Y. Mathias <vhymathias@gmail.com>
2021-08-17 20:32:52 -0300

    Added (redirect to login after new password) feature

--- src/components/ResetPasswordForm/ResetPasswordForm.js
+++ src/components/ResetPasswordForm/ResetPasswordForm.js
@@ -4,12 +4,14 @@ import { API_URL } from "../../constants";
 import { Loading } from "../../styles/common";
 import PasswordForm from "../PasswordForm";
 import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function ResetPasswordForm() {
     const location = useLocation();
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState();
     const [loading, setLoading] = useState(false);
+    const history = useHistory();
 
     async function resetPassword(event) {
         event.preventDefault();
@@ -18,7 +20,6 @@ function ResetPasswordForm() {
 
         if (password !== "") {
             const data = { token, password };
-
             try {
                 setLoading(true);
                 const response = await fetch(`${API_URL}/reset`, {
@@ -27,12 +28,19 @@ function ResetPasswordForm() {
                     body: JSON.stringify(data),
                 });
 
-                if (response.status === 204) {
-                    setMessage("Senha alterada com sucesso. Por favor, faça login.");
+                if ((response.status === 204) | (response.status === 200)) {
+                    setTimeout(() => {
+                        history.push("/login");
+                    }, 5000);
+                    let timer = 5;
+                    window.setInterval(() => {
+                        setMessage("Senha alterada com sucesso. Redirecionando para o login em " + timer + "segundos!");
+                        timer--;
+                    }, 1000);
                 } else if (response.status === 400) {
-                    setMessage("Link de redefinição de senha expirou.");
+                    setMessage("Link de redefinição de senha expirou. Tente novamente!");
                 } else {
-                    setMessage("Link de redefinição de senha inválido.");
+                    setMessage("Link de redefinição de senha inválido. Tente novamente!");
                 }
             } catch {
                 setMessage("Ops, ocorreu um problema na requisição. Tente novamente!");
