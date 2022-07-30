ffc109d320fd8c2953679c62fe5fb946a92c1293
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2022-07-30 15:54:18 -0300

    comentando pra subir pra main, descomentar no futuro

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://api.vcnafacul.com.br";
-export const API_URL = "http://localhost:3333";
+export const API_URL = "https://api.vcnafacul.com.br";
+//export const API_URL = "http://localhost:3333";
