ebf516321acca6e0047afa7eada7e7f7e894beab
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-05-23 22:12:57 -0300

    ajuste propriedade css

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,7 +7,7 @@ function Dashboard() {
         <Content>
             <Hero src={HeroImg} alt="Hero acenando para você" />
             <Text>
-                <h1 style={{ "font-weight": "500", padding: "8px 0" }}>Bem vindos a</h1>
+                <h1 style={{ fontWeight: "500", padding: "8px 0" }}>Bem vindos a</h1>
                 <h1>VOCÊ NA FACUL!</h1>
                 <Message>Lorem ipsum dolor sit amet, consectetur adipisicing elitr, sed diam nonumy.</Message>
             </Text>
