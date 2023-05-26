d6dd2f496f1f3f9fcadf968e4e70902b712e63b7
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-05-25 23:19:51 -0300

    remove espaco em branco

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,7 +1,6 @@
 import { AccordionTitle, Container } from "./styles";
 
 function Accordion({ isOpen, setStateOpen, titleClosed, titleOpened, children }) {
-
     return (
         <Container>
             <AccordionTitle onClick={setStateOpen}>{isOpen ? titleOpened : titleClosed}</AccordionTitle>
